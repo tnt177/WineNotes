328d5aab32919ed1375cb2375d9d68a3328b078b
TTORR <torres.tamara7@yahoo.com>
2022-04-11 16:52:49 -0500

    Added activity main layout, recyclerview and item view layout

--- app/src/main/java/com/example/winenotes/MainActivity.kt
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -13,9 +13,42 @@ import com.example.winenotes.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var adapter : MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerview.setLayoutManager(layoutManager)
+
+        val dividerItemDecoration = DividerItemDecoration(
+            applicationContext, layoutManager.getOrientation()
+        )
+        binding.recyclerview.addItemDecoration(dividerItemDecoration)
+
+        adapter = MyAdapter()
+        binding.recyclerview.setAdapter(adapter)
+    }
+    inner class MyViewHolder(val view: TextView) :
+        RecyclerView.ViewHolder(view) {
+
+    }
+    inner class MyAdapter :
+        RecyclerView.Adapter<MyViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item__view, parent, false) as TextView
+            return MyViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            return 0
+        }
     }
 }
