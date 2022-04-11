a0a323a12c0af2c8417564fb35088d16bc041293
TTORR <torres.tamara7@yahoo.com>
2022-04-11 16:41:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.winenotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.winenotes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
